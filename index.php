ac0fa5133d4ccf900043a770305f24522c968832
Nicolas Widart <n.widart@gmail.com>
2012-04-24 16:26:00 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,193 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset=utf-8>
+	<title>Gallerie</title>
+	<link rel="stylesheet" href="css/reset.css">
+	<link rel="stylesheet" href="css/960.css">
+	<link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+<header>
+	<div class="container_12">
+		<div class="grid_12">
+			<h1>My portfolio</h1>
+			<h2>Logo, print and web design. Browse & enjoy.</h2>
+		</div>
+	</div><!-- end container -->
+</header>
+<nav>
+	<div class="container_12">
+		<div class="grid_12">
+			<div class="ribbon">
+				<strong class="ribbon-content">
+					<ul>
+						<li class="active"><a href="">Everything</a></li>
+						<li><a href="">Print Design</a></li>
+						<li><a href="">Logo Design</a></li>
+						<li><a href="">Web Design</a></li>
+					</ul>
+				</strong>
+			</div>
+		</div>
+	</div><!-- end container -->
+</nav>
+<div class="container_12 projects">
+	<div class="grid_12">
+		<ul id="da-thumbs" class="da-thumbs">
+			<li>
+				<a href="">
+					<img src="images/projects/img1.jpg" alt="">
+					<div><span>Image 1</span></div>
+				</a>
+			</li>
+			<li>
+				<a href="">
+					<img src="images/projects/img2.jpg" alt="">
+					<div><span>Image 2</span></div>
+				</a>
+			</li>
+			<li>
+				<a href="">
+					<img src="images/projects/img3.jpg" alt="">
+					<div><span>Image 3</span></div>
+				</a>
+			</li>
+			<li>
+				<a href="">
+					<img src="images/projects/img4.jpg" alt="">
+					<div><span>Image 4</span></div>
+				</a>
+			</li>
+			<li>
+				<a href="">
+					<img src="images/projects/img5.jpg" alt="">
+					<div><span>Image 5</span></div>
+				</a>
+			</li>
+			<li>
+
+				<a href="">
+					<img src="images/projects/img6.jpg" alt="">
+					<div><span>Image 6</span></div>
+				</a>
+			</li>
+			<li>
+				<a href="">
+					<img src="images/projects/img7.jpg" alt="">
+					<div><span>Image 7</span></div>
+				</a>
+			</li>
+			<li>
+				<a href="">
+					<img src="images/projects/img8.jpg" alt="">
+					<div><span>Image 8</span></div>
+				</a>
+			</li>
+			<li>
+				<a href="">
+					<img src="images/projects/img9.jpg" alt="">
+					<div><span>Image 9</span></div>
+				</a>
+			</li>
+			<li>
+				<a href="">
+					<img src="images/projects/img10.jpg" alt="">
+					<div><span>Image 10</span></div>
+				</a>
+			</li>
+			<li>
+				<a href="">
+					<img src="images/projects/img11.jpg" alt="">
+					<div><span>Image 11</span></div>
+				</a>
+			</li>
+			<li>
+				<a href="">
+					<img src="images/projects/img12.jpg" alt="">
+					<div><span>Image 12</span></div>
+				</a>
+			</li>
+			<li>
+				<a href="">
+					<img src="images/projects/img13.jpg" alt="">
+					<div><span>Image 13</span></div>
+				</a>
+			</li>
+			<li>
+				<a href="">
+					<img src="images/projects/img14.jpg" alt="">
+					<div><span>Image 14</span></div>
+				</a>
+			</li>
+			<li>
+				<a href="">
+					<img src="images/projects/img15.jpg" alt="">
+					<div><span>Image 15</span></div>
+				</a>
+			</li>
+			<li>
+				<a href="">
+					<img src="images/projects/img16.jpg" alt="">
+					<div><span>Image 16</span></div>
+				</a>
+			</li>
+			<li>
+				<a href="">
+					<img src="images/projects/img17.jpg" alt="">
+					<div><span>Image 17</span></div>
+				</a>
+			</li>
+			<li>
+				<a href="">
+					<img src="images/projects/img18.jpg" alt="">
+					<div><span>Image 18</span></div>
+				</a>
+			</li>
+			<li>
+				<a href="">
+					<img src="images/projects/img19.jpg" alt="">
+					<div><span>Image 19</span></div>
+				</a>
+			</li>
+			<li>
+				<a href="">
+					<img src="images/projects/img20.jpg" alt="">
+					<div><span>Image 20</span></div>
+			</a>
+			</li>
+			<li>
+				<a href="">
+					<img src="images/projects/img21.jpg" alt="">
+					<div><span>Image 21</span></div>
+				</a>
+			</li>
+			<li>
+				<a href="">
+					<img src="images/projects/img22.jpg" alt="">
+					<div><span>Image 22</span></div>
+				</a>
+			</li>
+		</ul>
+	</div>
+</div>
+<div class="container_12 footer">
+	<div class="grid_12">
+		<p>
+			© TP WEB - HEAJ 2012
+		</p>
+	</div>
+</div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+
+<script type="text/javascript" src="js/jquery.hoverdir.js"></script>
+<script type="text/javascript">
+			$(function() {
+				$('#da-thumbs > li').hoverdir();
+
+			});
+		</script>
+
+</body>
+</html>
