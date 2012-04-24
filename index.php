213d59d61a088d54461c24dbc468fc07b2ab824a
Nicolas Widart <n.widart@gmail.com>
2012-04-24 17:51:08 +0200

    Add | Footer links

--- index.php
+++ index.php
@@ -176,7 +176,7 @@
 <div class="container_12 footer">
 	<div class="grid_12">
 		<p>
-			© TP WEB - HEAJ 2012
+			© TP WEB - HEAJ 2012 | Created by <a href="http://www.nicolaswidart.com" target="_blank">Nicolas Widart</a> | <a href="https://github.com/nWidart/TPWeb_Gallerie/" target="_blank">Code on GitHub</a>
 		</p>
 	</div>
 </div>
@@ -196,45 +196,45 @@ $(document).ready(function() {
 	// cache container
 	var $container = $('#da-thumbs');
 	$container.imagesLoaded( function(){
-	// initialize isotope
-	$container.isotope({
-		itemSelector: '.element',
-		layoutMode : 'masonry',
-		masonry : {
-			columnWidth: $('.element').width() / 8,
-		},
-		resizable : 'false',
-		animationEngine : 'jquery',
-		animationOptions: {
-			duration : 500,
-			easing: 'linear',
-			queue : false
-		}
+		// initialize isotope
+		$container.isotope({
+			itemSelector: '.element',
+			layoutMode : 'masonry',
+			masonry : {
+				columnWidth: $('.element').width() / 8,
+			},
+			resizable : 'false',
+			animationEngine : 'jquery',
+			animationOptions: {
+				duration : 500,
+				easing: 'linear',
+				queue : false
+			}
 
-	});
+		});
 
-	// filter items when filter link is clicked
-	$('.filters a').click(function(){
-		event.preventDefault();
-	  var selector = $(this).attr('data-filter');
-	  $container.isotope({ filter: selector });
-	  // Adding the active class
-	  var $filterType = $('.filters li.active a').attr('class');
-	  $('.filters li').removeClass('active');
-	  //var $filterType = $(this).attr('class');
-	  $(this).parent().addClass('active');
-	  if ($filterType == 'all') {
-			// assign all li items to the $filteredData var when
-			// the 'All' filter option is clicked
-			var $filteredData = $data.find('li');
-		}
-		else {
-			// find all li elements that have our required $filterType
-			// values for the data-type element
-			var $filteredData = $data.find('li[data-type=' + $filterType + ']');
-		}
-	  return false;
-	});
+		// filter items when filter link is clicked
+		$('.filters a').click(function(){
+			event.preventDefault();
+		  var selector = $(this).attr('data-filter');
+		  $container.isotope({ filter: selector });
+		  // Adding the active class
+		  var $filterType = $('.filters li.active a').attr('class');
+		  $('.filters li').removeClass('active');
+		  //var $filterType = $(this).attr('class');
+		  $(this).parent().addClass('active');
+		  if ($filterType == 'all') {
+				// assign all li items to the $filteredData var when
+				// the 'All' filter option is clicked
+				var $filteredData = $data.find('li');
+			}
+			else {
+				// find all li elements that have our required $filterType
+				// values for the data-type element
+				var $filteredData = $data.find('li[data-type=' + $filterType + ']');
+			}
+		  return false;
+		});
 	});
 
 });
