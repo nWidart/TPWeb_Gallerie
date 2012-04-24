2254a3c525abac62ce669556006ad805c84f05c5
Nicolas Widart <n.widart@gmail.com>
2012-04-24 16:47:59 +0200

    Added a lightbox

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
 	<link rel="stylesheet" href="css/reset.css">
 	<link rel="stylesheet" href="css/960.css">
 	<link rel="stylesheet" href="css/main.css">
+	<link rel="stylesheet" href="css/jquery.lightbox-0.5.css" />
+
 </head>
 <body>
 <header>
@@ -36,134 +38,134 @@
 	<div class="grid_12">
 		<ul id="da-thumbs" class="da-thumbs">
 			<li>
-				<a href="">
+				<a href="images/projects/img1.jpg">
 					<img src="images/projects/img1.jpg" alt="">
 					<div><span>Image 1</span></div>
 				</a>
 			</li>
 			<li>
-				<a href="">
+				<a href="images/projects/img2.jpg">
 					<img src="images/projects/img2.jpg" alt="">
 					<div><span>Image 2</span></div>
 				</a>
 			</li>
 			<li>
-				<a href="">
+				<a href="images/projects/img3.jpg">
 					<img src="images/projects/img3.jpg" alt="">
 					<div><span>Image 3</span></div>
 				</a>
 			</li>
 			<li>
-				<a href="">
+				<a href="images/projects/img4.jpg">
 					<img src="images/projects/img4.jpg" alt="">
 					<div><span>Image 4</span></div>
 				</a>
 			</li>
 			<li>
-				<a href="">
+				<a href="images/projects/img5.jpg">
 					<img src="images/projects/img5.jpg" alt="">
 					<div><span>Image 5</span></div>
 				</a>
 			</li>
 			<li>
 
-				<a href="">
+				<a href="images/projects/img6.jpg">
 					<img src="images/projects/img6.jpg" alt="">
 					<div><span>Image 6</span></div>
 				</a>
 			</li>
 			<li>
-				<a href="">
+				<a href="images/projects/img7.jpg">
 					<img src="images/projects/img7.jpg" alt="">
 					<div><span>Image 7</span></div>
 				</a>
 			</li>
 			<li>
-				<a href="">
+				<a href="images/projects/img8.jpg">
 					<img src="images/projects/img8.jpg" alt="">
 					<div><span>Image 8</span></div>
 				</a>
 			</li>
 			<li>
-				<a href="">
+				<a href="images/projects/img9.jpg">
 					<img src="images/projects/img9.jpg" alt="">
 					<div><span>Image 9</span></div>
 				</a>
 			</li>
 			<li>
-				<a href="">
+				<a href="images/projects/img10.jpg">
 					<img src="images/projects/img10.jpg" alt="">
 					<div><span>Image 10</span></div>
 				</a>
 			</li>
 			<li>
-				<a href="">
+				<a href="images/projects/img11.jpg">
 					<img src="images/projects/img11.jpg" alt="">
 					<div><span>Image 11</span></div>
 				</a>
 			</li>
 			<li>
-				<a href="">
+				<a href="images/projects/img12.jpg">
 					<img src="images/projects/img12.jpg" alt="">
 					<div><span>Image 12</span></div>
 				</a>
 			</li>
 			<li>
-				<a href="">
+				<a href="images/projects/img13.jpg">
 					<img src="images/projects/img13.jpg" alt="">
 					<div><span>Image 13</span></div>
 				</a>
 			</li>
 			<li>
-				<a href="">
+				<a href="images/projects/img14.jpg">
 					<img src="images/projects/img14.jpg" alt="">
 					<div><span>Image 14</span></div>
 				</a>
 			</li>
 			<li>
-				<a href="">
+				<a href="images/projects/img15.jpg">
 					<img src="images/projects/img15.jpg" alt="">
 					<div><span>Image 15</span></div>
 				</a>
 			</li>
 			<li>
-				<a href="">
+				<a href="images/projects/img16.jpg">
 					<img src="images/projects/img16.jpg" alt="">
 					<div><span>Image 16</span></div>
 				</a>
 			</li>
 			<li>
-				<a href="">
+				<a href="images/projects/img17.jpg">
 					<img src="images/projects/img17.jpg" alt="">
 					<div><span>Image 17</span></div>
 				</a>
 			</li>
 			<li>
-				<a href="">
+				<a href="images/projects/img18.jpg">
 					<img src="images/projects/img18.jpg" alt="">
 					<div><span>Image 18</span></div>
 				</a>
 			</li>
 			<li>
-				<a href="">
+				<a href="images/projects/img19.jpg">
 					<img src="images/projects/img19.jpg" alt="">
 					<div><span>Image 19</span></div>
 				</a>
 			</li>
 			<li>
-				<a href="">
+				<a href="images/projects/img20.jpg">
 					<img src="images/projects/img20.jpg" alt="">
 					<div><span>Image 20</span></div>
 			</a>
 			</li>
 			<li>
-				<a href="">
+				<a href="images/projects/img21.jpg">
 					<img src="images/projects/img21.jpg" alt="">
 					<div><span>Image 21</span></div>
 				</a>
 			</li>
 			<li>
-				<a href="">
+				<a href="images/projects/img22.jpg">
 					<img src="images/projects/img22.jpg" alt="">
 					<div><span>Image 22</span></div>
 				</a>
@@ -180,14 +182,14 @@
 </div>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-
+<script type="text/javascript" src="js/jquery.lightbox-0.5.min.js"></script>
 <script type="text/javascript" src="js/jquery.hoverdir.js"></script>
 <script type="text/javascript">
-			$(function() {
-				$('#da-thumbs > li').hoverdir();
-
-			});
-		</script>
+	$(function() {
+		$('#da-thumbs > li').hoverdir();
+		$('#da-thumbs a').lightBox({fixedNavigation:true});
+	});
+</script>
 
 </body>
 </html>
