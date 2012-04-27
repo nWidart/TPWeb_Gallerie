74ad777b0639ee810710f9188dce2a65302ab4a0
Nicolas Widart <n.widart@gmail.com>
2012-04-27 15:44:02 +0200

    Fix | Moved the scripts from index.php to scripts.js

--- index.php
+++ index.php
@@ -38,134 +38,134 @@
 	<div class="grid_12">
 		<ul id="da-thumbs" class="da-thumbs">
 			<li class="element print">
-				<a href="images/projects/img1.jpg">
+				<a href="images/projects/full_size/1.jpg">
 					<img src="images/projects/img1.jpg" alt="">
 					<div><span>Image 1</span></div>
 				</a>
 			</li>
 			<li class="element logo">
-				<a href="images/projects/img2.jpg">
+				<a href="images/projects/full_size/2.jpg">
 					<img src="images/projects/img2.jpg" alt="">
 					<div><span>Image 2</span></div>
 				</a>
 			</li>
 			<li class="element web">
-				<a href="images/projects/img3.jpg">
+				<a href="images/projects/full_size/3.jpg">
 					<img src="images/projects/img3.jpg" alt="">
 					<div><span>Image 3</span></div>
 				</a>
 			</li>
 			<li class="element print">
-				<a href="images/projects/img4.jpg">
+				<a href="images/projects/full_size/4.jpg">
 					<img src="images/projects/img4.jpg" alt="">
 					<div><span>Image 4</span></div>
 				</a>
 			</li>
 			<li class="element print">
-				<a href="images/projects/img5.jpg">
+				<a href="images/projects/full_size/5.jpg">
 					<img src="images/projects/img5.jpg" alt="">
 					<div><span>Image 5</span></div>
 				</a>
 			</li>
 			<li class="element logo">
 
-				<a href="images/projects/img6.jpg">
+				<a href="images/projects/full_size/6.jpg">
 					<img src="images/projects/img6.jpg" alt="">
 					<div><span>Image 6</span></div>
 				</a>
 			</li>
 			<li class="element print">
-				<a href="images/projects/img7.jpg">
+				<a href="images/projects/full_size/7.jpg">
 					<img src="images/projects/img7.jpg" alt="">
 					<div><span>Image 7</span></div>
 				</a>
 			</li>
 			<li class="element logo">
-				<a href="images/projects/img8.jpg">
+				<a href="images/projects/full_size/8.jpg">
 					<img src="images/projects/img8.jpg" alt="">
 					<div><span>Image 8</span></div>
 				</a>
 			</li>
 			<li class="element print">
-				<a href="images/projects/img9.jpg">
+				<a href="images/projects/full_size/9.jpg">
 					<img src="images/projects/img9.jpg" alt="">
 					<div><span>Image 9</span></div>
 				</a>
 			</li>
 			<li class="element web">
-				<a href="images/projects/img10.jpg">
+				<a href="images/projects/full_size/10.jpg">
 					<img src="images/projects/img10.jpg" alt="">
 					<div><span>Image 10</span></div>
 				</a>
 			</li>
 			<li class="element logo">
-				<a href="images/projects/img11.jpg">
+				<a href="images/projects/full_size/11.jpg">
 					<img src="images/projects/img11.jpg" alt="">
 					<div><span>Image 11</span></div>
 				</a>
 			</li>
 			<li class="element web">
-				<a href="images/projects/img12.jpg">
+				<a href="images/projects/full_size/12.jpg">
 					<img src="images/projects/img12.jpg" alt="">
 					<div><span>Image 12</span></div>
 				</a>
 			</li>
 			<li class="element print">
-				<a href="images/projects/img13.jpg">
+				<a href="images/projects/full_size/13.jpg">
 					<img src="images/projects/img13.jpg" alt="">
 					<div><span>Image 13</span></div>
 				</a>
 			</li>
 			<li class="element logo">
-				<a href="images/projects/img14.jpg">
+				<a href="images/projects/full_size/14.jpg">
 					<img src="images/projects/img14.jpg" alt="">
 					<div><span>Image 14</span></div>
 				</a>
 			</li>
 			<li class="element web">
-				<a href="images/projects/img15.jpg">
+				<a href="images/projects/full_size/15.jpg">
 					<img src="images/projects/img15.jpg" alt="">
 					<div><span>Image 15</span></div>
 				</a>
 			</li>
 			<li class="element logo">
-				<a href="images/projects/img16.jpg">
+				<a href="images/projects/full_size/16.jpg">
 					<img src="images/projects/img16.jpg" alt="">
 					<div><span>Image 16</span></div>
 				</a>
 			</li>
 			<li class="element print">
-				<a href="images/projects/img17.jpg">
+				<a href="images/projects/full_size/17.jpg">
 					<img src="images/projects/img17.jpg" alt="">
 					<div><span>Image 17</span></div>
 				</a>
 			</li>
 			<li class="element web">
-				<a href="images/projects/img18.jpg">
+				<a href="images/projects/full_size/18.jpg">
 					<img src="images/projects/img18.jpg" alt="">
 					<div><span>Image 18</span></div>
 				</a>
 			</li>
 			<li class="element print">
-				<a href="images/projects/img19.jpg">
+				<a href="images/projects/full_size/19.jpg">
 					<img src="images/projects/img19.jpg" alt="">
 					<div><span>Image 19</span></div>
 				</a>
 			</li>
 			<li class="element logo">
-				<a href="images/projects/img20.jpg">
+				<a href="images/projects/full_size/20.jpg">
 					<img src="images/projects/img20.jpg" alt="">
 					<div><span>Image 20</span></div>
 			</a>
 			</li>
 			<li class="element web">
-				<a href="images/projects/img21.jpg">
+				<a href="images/projects/full_size/21.jpg">
 					<img src="images/projects/img21.jpg" alt="">
 					<div><span>Image 21</span></div>
 				</a>
 			</li>
 			<li class="element web">
-				<a href="images/projects/img22.jpg">
+				<a href="images/projects/full_size/22.jpg">
 					<img src="images/projects/img22.jpg" alt="">
 					<div><span>Image 22</span></div>
 				</a>
@@ -185,60 +185,7 @@
 <script type="text/javascript" src="js/jquery.lightbox-0.5.min.js"></script>
 <script type="text/javascript" src="js/jquery.hoverdir.js"></script>
 <script type="text/javascript" src="https://raw.github.com/desandro/isotope/master/jquery.isotope.js"></script>
-<script type="text/javascript">
-	$(function() {
-		$('#da-thumbs > li').hoverdir();
-		$('#da-thumbs a').lightBox({fixedNavigation:true});
-	});
-</script>
-<script type="text/javascript">
-$(document).ready(function() {
-	// cache container
-	var $container = $('#da-thumbs');
-	$container.imagesLoaded( function(){
-		// initialize isotope
-		$container.isotope({
-			itemSelector: '.element',
-			layoutMode : 'masonry',
-			masonry : {
-				columnWidth: $('.element').width() / 8,
-			},
-			resizable : 'false',
-			animationEngine : 'jquery',
-			animationOptions: {
-				duration : 500,
-				easing: 'linear',
-				queue : false
-			}
-
-		});
-
-		// filter items when filter link is clicked
-		$('.filters a').click(function(){
-			event.preventDefault();
-		  var selector = $(this).attr('data-filter');
-		  $container.isotope({ filter: selector });
-		  // Adding the active class
-		  var $filterType = $('.filters li.active a').attr('class');
-		  $('.filters li').removeClass('active');
-		  //var $filterType = $(this).attr('class');
-		  $(this).parent().addClass('active');
-		  if ($filterType == 'all') {
-				// assign all li items to the $filteredData var when
-				// the 'All' filter option is clicked
-				var $filteredData = $data.find('li');
-			}
-			else {
-				// find all li elements that have our required $filterType
-				// values for the data-type element
-				var $filteredData = $data.find('li[data-type=' + $filterType + ']');
-			}
-		  return false;
-		});
-	});
-
-});
-</script>
+<script type="text/javascript" src="js/scripts.js"></script>
 
 </body>
 </html>
---
+++ js/scripts.js
@@ -0,0 +1,57 @@
+/* ------------------------------------------------------------------------------------------------------------------------
+ * Loading the captions and lightbox
+ */
+$(document).ready(function() {
+		$('#da-thumbs > li').hoverdir();
+		$('#da-thumbs a').lightBox({fixedNavigation:true});
+});
+
+/* ------------------------------------------------------------------------------------------------------------------------
+ * Isotop init
+ */
+$(document).ready(function() {
+	// cache container
+	var $container = $('#da-thumbs');
+	$container.imagesLoaded( function(){
+		// initialize isotope
+		$container.isotope({
+			itemSelector: '.element',
+			layoutMode : 'masonry',
+			masonry : {
+				columnWidth: $('.element').width() / 8,
+			},
+			resizable : 'false',
+			animationEngine : 'jquery',
+			animationOptions: {
+				duration : 500,
+				easing: 'linear',
+				queue : false
+			}
+
+		});
+
+		// filter items when filter link is clicked
+		$('.filters a').click(function(){
+			event.preventDefault();
+		  var selector = $(this).attr('data-filter');
+		  $container.isotope({ filter: selector });
+		  // Adding the active class
+		  var $filterType = $('.filters li.active a').attr('class');
+		  $('.filters li').removeClass('active');
+		  //var $filterType = $(this).attr('class');
+		  $(this).parent().addClass('active');
+		  if ($filterType == 'all') {
+				// assign all li items to the $filteredData var when
+				// the 'All' filter option is clicked
+				var $filteredData = $data.find('li');
+			}
+			else {
+				// find all li elements that have our required $filterType
+				// values for the data-type element
+				var $filteredData = $data.find('li[data-type=' + $filterType + ']');
+			}
+		  return false;
+		});
+	});
+
+});
